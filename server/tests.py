438ae374eb2bd5dc312b07efc6073e790aa99f65
IamEinstein <rishit_verma@outlook.com>
2021-03-29 13:58:05 +0530

    Added tests

--- server/tests.py
+++ server/tests.py
@@ -1,10 +1,25 @@
 from django.test import TestCase
-import os
+from .models import User
 # Create your tests here.
+import bcrypt
+import random
 
 
-class TestUser(TestCase):
-    def test_env_vars(self):
-        for i in os.environ:
-            print(i)
-        print(os.getenv("GITHUB_WORKFLOW"))
+class UserTestCase(TestCase):
+    uuid = ""
+
+    def setUp(self):
+        new_user = User.objects.create()
+        new_user.username = "Test User 1"
+        passwords = ['hunt', 'fish', 'beg', 'work',
+                     'poke2', 'alphanumericallll1235', 'ææhmm']
+        self.pwd = random.choice(passwords)
+        hash = bcrypt.hashpw(bytes(self.pwd, 'utf-8'), bcrypt.gensalt())
+        new_user.password = hash
+        self.uuid = new_user.unique_id
+        new_user.save()
+
+    def test_overall(self):
+        user = User.objects.get(unique_id=self.uuid)
+        self.assertEqual(bcrypt.checkpw(
+            bytes(self.pwd, 'utf-8'), user.password), True)
