ea45a96de08309234169664177045ab1e0bb8905
IamEinstein <rishit_verma@outlook.com>
2021-04-03 16:30:48 +0530

    Update Settings.py

--- bank/settings.py
+++ bank/settings.py
@@ -214,3 +214,11 @@ LOGGING = {
         }
     }
 }
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
+}
