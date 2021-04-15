f33f23781e8f270b42f7264068e1da8e36141f9a
IamEinstein <63300310+IamEinstein@users.noreply.github.com>
2021-04-15 13:37:22 +0530

    Added transaction history

--- server/hash.py
+++ server/hash.py
@@ -1,23 +1,24 @@
+import json
+# from .models import User
+# # ! Hash
+# import bcrypt
+# pwd = bytes(input("Password ??"), 'utf-8')
 
-# ! Hash
-import bcrypt
-pwd = bytes(input("Password ??"), 'utf-8')
+# hash = bcrypt.hashpw(pwd, bcrypt.gensalt())
 
-hash = bcrypt.hashpw(pwd, bcrypt.gensalt())
-
-hash = b'$2b$12$889xpznjPrcEnoIRl11LCeVyidvkUUs7DXnfmSWhw9dbf68nkGPTm'
-# print(hash)
-if bcrypt.checkpw(pwd, hash):
-    print("True")
-else:
-    print("false")
+# hash = b'$2b$12$889xpznjPrcEnoIRl11LCeVyidvkUUs7DXnfmSWhw9dbf68nkGPTm'
+# # print(hash)
+# if bcrypt.checkpw(pwd, hash):
+#     print("True")
+# else:
+#     print("false")
 
 
-def check_user(password, hashed_pswd):
-    if bcrypt.checkpw(password, hashed_pswd):
-        return True
-    else:
-        return False
+# def check_user(password, hashed_pswd):
+#     if bcrypt.checkpw(password, hashed_pswd):
+#         return True
+#     else:
+#         return False
 
 # # ! Encrypt-Decrypt
 # from cryptography.fernet import Fernet
@@ -30,3 +31,18 @@ def check_user(password, hashed_pswd):
 # decoded_string = str(a.decrypt(coded_string))
 # print(decoded_string)
 # print(type(decoded_string))
+
+# myModel = User()
+# listIWantToStore = [1, 2, 3, 4, 5, 'hello']
+# json_list = json.dumps(listIWantToStore)
+# print(type(json_list))
+# jsonDec = json.decoder.JSONDecoder()
+# real_list = jsonDec.decode("")
+# print(type(real_list))
+# real_list.append('new_item')
+# print(real_list)
+# json_list = json.dumps(listIWantToStore)
+# print(type(json_list))
+# print(json_list)
+
+# myModel.save()
