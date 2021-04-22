2d4ec9ba00c71fb03299a64d7e9c7d0ec58b111b
IamEinstein <63300310+IamEinstein@users.noreply.github.com>
2021-04-22 21:26:48 +0530

    Added redirect

--- redirect/models.py
+++ redirect/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class Redirect(models.Model):
+    link1 = models.CharField(max_length=10485700)
+    unique_id = models.UUIDField(
+        default=uuid.uuid4, unique=True, editable=False)
+    link2 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link3 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link4 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link5 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link6 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link7 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link8 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link9 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
+    link10 = models.CharField(
+        max_length=10485700, editable=True, null=True, default=None)
--- redirect/urls.py
+++ redirect/urls.py
@@ -3,5 +3,8 @@ from .views import *
 app_name = "redirect"
 urlpatterns = [
     path('', index, name="index"),
+    path('link', link, name="create_link"),
+    path('redirect/<str:id>', redirect_pg, name="redirect"),
+    path('cs', create_link, name="cs")
 
 ]
