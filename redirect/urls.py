7d9c1646afa0ad8879666bf8da56105632ae29de
IamEinstein <63300310+IamEinstein@users.noreply.github.com>
2021-05-03 10:09:16 +0530

    Small Update

--- redirect/urls.py
+++ redirect/urls.py
@@ -4,8 +4,8 @@ app_name = "redirect"
 urlpatterns = [
     path('', index, name="index"),
     path('link', link, name="create_link"),
-    path('redirect/<str:id>', redirect_pg, name="redirect"),
+    path('redirect/<str:id>/', redirect_pg, name="redirect"),
     path('cs', create_link, name="cs"),
-    path('r', redirect_link, name="redirect_link")
+    path('r/<str:id>/', redirect_link, name="redirect_link")
 
 ]
